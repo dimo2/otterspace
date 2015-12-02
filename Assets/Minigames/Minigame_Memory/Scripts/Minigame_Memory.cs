8d2d692a8d0e898ed4d977f986630fec40c1a842
JonasZimmer <Zimmer-Jonas@web.de>
2015-12-03 00:02:35 +0100

    Minigame 2 - Memory
    Erste Version, Platzierung passt und Umdrehen kann man die Karten auch
    schon. Aber noch keine Sieg/Verlust Bedingungen.

--- Assets/Minigames/Minigame_Memory/Scripts/Minigame_Memory.cs
+++ Assets/Minigames/Minigame_Memory/Scripts/Minigame_Memory.cs
@@ -9,14 +9,56 @@ public class Minigame_Memory : MiniGame
 	public Sprite	Kartenhintergrund;
 	public GameObject prefabKarte;
 
+	public int rows;
+	public int columns;
+
 	void Start()
 	{
 		Karten =
 			new List<Minigame_Memory_Karte>();
+		CreateField();
 	}
 
 	private void CreateField()
 	{
+		//erstellt für jede Grafik 2 Karten
+		foreach (Sprite s in Kartenmotive)
+		{
+			for (int i=0; i < 2; i++)
+			{
+				Minigame_Memory_Karte k = 
+					GameObject.Instantiate(prefabKarte).GetComponent<Minigame_Memory_Karte>();
+				k.Create(s, Kartenhintergrund);
+				Karten.Add(k);
+				k.gameObject.transform.parent = transform;
+				k.gameObject.transform.localPosition = 
+					new Vector3(0,10,0);
+			}
+		}
+		ShuffleField();
+	}
 
+	private void ShuffleField()
+	{
+		int placed = 0;
+		for (int i = 0; i < rows; i++)
+			for (int j = 0; j < columns; j++)
+		{
+			if (placed >= Karten.Count)
+				return;
+			GameObject go;
+			for(;;)
+			{
+				int r = Random.Range(0,Karten.Count);
+				go = Karten[r].gameObject;
+				if (go.transform.localPosition.y == 10)
+					break;
+			}
+			go.transform.localPosition = 
+				new Vector3(
+					-((-0.5f+(columns/2))*2.4f) + (j*2.4f),
+					2f - (i * 2.2f));
+			placed++;
+		}
 	}
 }
--- Assets/Minigames/Minigame_Memory/Scripts/Minigame_Memory_Karte.cs
+++ Assets/Minigames/Minigame_Memory/Scripts/Minigame_Memory_Karte.cs
@@ -5,14 +5,60 @@ public class Minigame_Memory_Karte : MonoBehaviour
 {
 	private Sprite back;
 	private Sprite front;
+	private SpriteRenderer sr;
 
 	public bool isFlipped;
+	public float flipTime;
 
 	public void Create(Sprite _f, Sprite _b)
 	{
 		back = _b;
 		front = _f;
 
-		isFlipped = true;
+		isFlipped = false;
+		sr = GetComponent<SpriteRenderer>();
+	}
+
+	public void OnMouseDown()
+	{
+		Flip();
+	}
+
+	public void Flip()
+	{
+		StopCoroutine(Flipping());
+		StartCoroutine(Flipping());
+	}
+
+	private void SwitchMotive()
+	{
+		if (!isFlipped)
+			sr.sprite = front;
+		else
+			sr.sprite = back;
+		isFlipped = !isFlipped;
+	}
+
+	private IEnumerator Flipping()
+	{
+		float elapsedTime = 0;
+		float startPoint = 
+			transform.localScale.y; //Falls während des Flippens neu geflippt wird
+		while (elapsedTime < flipTime)
+		{
+			transform.localScale = 
+				new Vector3(Mathf.Lerp(startPoint, 0, (elapsedTime/flipTime)), 1, 1);
+			elapsedTime += Time.deltaTime;
+			yield return new WaitForEndOfFrame();
+		}
+		SwitchMotive();
+		elapsedTime = 0;
+		while (elapsedTime < flipTime)
+		{
+			transform.localScale = 
+				new Vector3(Mathf.Lerp(0, 1, (elapsedTime/flipTime)), 1, 1);
+			elapsedTime += Time.deltaTime;
+			yield return new WaitForEndOfFrame();
+		}
 	}
 }
