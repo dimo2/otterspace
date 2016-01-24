6bac2f94a4024cf16d1e8384a1a2b9640a0b2f23
JonasZimmer <Zimmer-Jonas@web.de>
2016-01-24 15:15:35 +0100

    Fix for RandomInvader

--- Assets/Minigames/Minigame_SpaceInvaders/Scripts/Minigame_SI_randomshot.cs
+++ Assets/Minigames/Minigame_SpaceInvaders/Scripts/Minigame_SI_randomshot.cs
@@ -3,22 +3,36 @@ using System.Collections;
 
 public class Minigame_SI_randomshot : MonoBehaviour {
 
+    public float cooldown;
+    private float dTime;
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+    {
+        dTime = 0;
+        //cooldown eventuell mit Time Factor verknüpfen hier?
+        //cooldown *= timeFactor;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+        dTime += Time.deltaTime;
+        if (dTime > cooldown)
+        {
+            dTime = 0;
+            Transform attackOrigin = 
+                RandomInvader();
+            //Jetzt machst du hier was mit dem Transform
+        }
 	}
-    /*
-    Transform RandomInvader() {                 // soll einen RandomInvader zurück geben damit dieser nach einen bestimmten Cooldown schießt(in Update)
-        var allInvaders = GameObject.FindGameObjectsWithTag("Gegner") < Transform >;
+    
+    Transform RandomInvader() 
+    {                 
+        GameObject[] allInvaders = GameObject.FindGameObjectsWithTag("Gegner");
 		if (allInvaders.Length == 0) {
 			return null;
 		} else {
-			return allInvaders[Random.Range(0, allInvaders.Length-1)];
+			return allInvaders[Random.Range(0, allInvaders.Length-1)].transform;
 		}
-	}*/
+	}	
 }
