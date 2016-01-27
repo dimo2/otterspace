f228ecb05911f3b61707bc6dc94b7043ee265841
NikoHb <niko.haberer@gmail.com>
2016-01-27 19:24:09 +0100

    Noch ein paar Verbesserungen
    Highscore bleibt jetzt gespeichert, wenn man die App schließt,
    Highscore kann in den Einstellungen angeschaut werden,
    Fische lassen sich jetzt besser anklicken,
    und ich hab noch ein paar Debug Logs auskommentiert!

---
+++ Assets/Interface/Preferences/HighscoreScreen/Scripts/HighScoreScreen.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighScoreScreen : MonoBehaviour {
+
+	private float highscore;
+
+	// Use this for initialization
+	void Start () {
+
+		transform.GetComponentInChildren<Canvas> ().worldCamera = Camera.main;
+
+        if (PlayerPrefs.HasKey("highscore")) highscore = PlayerPrefs.GetFloat("highscore");
+        else highscore = 0;
+
+        showScore ();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	private void showScore() {
+        //Debug.Log(GameObject.Find("Score_Text"));
+		GameObject.Find("Score_Text").GetComponent<UnityEngine.UI.Text>().text = "Your Highscore";
+		GameObject.Find("Score_Score").GetComponent<UnityEngine.UI.Text>().text = highscore.ToString();
+	}
+
+    void OnMouseDown() {
+		Destroy (gameObject);
+	}
+}
--- Assets/Interface/Preferences/Scripts/Preferences_Highscore.cs
+++ Assets/Interface/Preferences/Scripts/Preferences_Highscore.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class Preferences_Highscore : MonoBehaviour {
 
+    public GameObject HighscoreScreen;
+    private GameObject screen;
+    private bool clicked;
+
 	// Use this for initialization
 	void Start () {
-	
+        clicked = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void OnMouseDown()
+    {
+        if (transform.parent.Find("HighScoreScreen(Clone)") == null)
+        {
+            screen = GameObject.Instantiate(HighscoreScreen);
+            screen.transform.parent = transform.parent;
+        }
+    }
 }
